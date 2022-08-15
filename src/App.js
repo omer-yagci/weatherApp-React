10c73be827c271509a79cded29ea3e10a3401e08
khan <omeryagci3295@gmail.com>
2022-08-16 00:28:56 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "./scss/App.scss";
 import Main from "./components/Main/Main";
 function App() {
   return (
-    <div className="App">
+    <>
       <Input />
       <Main />
       <ToastContainer />
-    </div>
+    </>
   );
 }
 
